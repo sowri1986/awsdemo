f11786b9cb063fdf3c84373d1a3bb5ffd6d70ad4
sowri1986 <54801046+sowri1986@users.noreply.github.com>
2023-06-14 14:14:24 +0100

    added child pages

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+import pandas as pd
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def upload_file(uploaded_file, bucket,fileName):
+    
+    # Upload the file
+    s3_client = boto3.client('s3')
+    
+    try:
+        response = s3_client.upload_fileobj(uploaded_file, bucket, fileName)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+uploaded_file = st.file_uploader("Choose a file")
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if(st.button('Upload to Bucket')):
+    #st.write(os.path.basename(uploaded_file.name))
+    upload_file(uploaded_file,"bit2terabyte",uploaded_file.name)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def hello_s3():
+    """
+    Use the AWS SDK for Python (Boto3) to create an Amazon Simple Storage Service
+    (Amazon S3) resource and list the buckets in your account.
+    This example uses the default settings specified in your shared credentials
+    and config files.
+    """
+    s3_resource = boto3.resource('s3')
+    st.write("Hello, Amazon S3! Let's list your buckets:")
+    for bucket in s3_resource.buckets.all():
+        st.write(f"\t{bucket.name}")
+
+if __name__ == '__main__':
+    hello_s3()
